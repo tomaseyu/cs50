e005ac46aa16ac5ee802009ca5dd05f48d1f62f5
radaxsly <72070675+tomaseyu@users.noreply.github.com>
2022-11-09 18:54:04 +0100

    Create hello.c

---
+++ problems/2022/x/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    printf("hello, worldsubmit50 cs50/problems/2022/x/hello\n");
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
+}
